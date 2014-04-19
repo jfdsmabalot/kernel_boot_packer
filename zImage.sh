a6d4169556cd51cb4d9c0aafb5a7fd3018b0def0
kiko <jfdsmabalot@gmail.com>
2014-04-19 18:45:28 +0800

    Boot Scripts Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

---
+++ zImage.sh
@@ -0,0 +1,17 @@
+./scripts/extract-ramdisk.pl boot.img
+./tools/mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+cmd_line=`scripts/get_cmdline`
+ramdisk_addr=`scripts/get_ramdisk_addr`
+base=`scripts/get_kernel_base_addr`
+
+if [ "$cmd_line" == "" ]
+then
+  ./tools/mkbootimg --kernel zImage --ramdisk ramdisk.gz -o arkenstone.img --base $base $ramdisk_params
+else
+  ./tools/mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "$cmd_line" -o arkenstone.img --base $base $ramdisk_params
+fi
+
+
+rm -r boot.img-ramdisk
+rm ramdisk.gz
+
